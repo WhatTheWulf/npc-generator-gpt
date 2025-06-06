f921dc4ed7ab191433c1ca88ea3d1a80c721d5aa
WhatTheWulf <waldmann.pp@gmail.com>
2025-06-06 18:21:33 +0200

    Update npc-generator.js

--- scripts/npc-generator.js
+++ scripts/npc-generator.js
@@ -82,8 +82,9 @@ async function generateNPC(promptText) {
 
     const npcJSON = JSON.parse(jsonString);
     // Entferne oder ersetze ungültige _id
-    if (npcJSON._id && (!/^[a-zA-Z0-9]{16}$/.test(npcJSON._id))) 
-    {    delete npcJSON._id; // Entferne es, Foundry erstellt automatisch eine neue gültige ID    }
+    if (npcJSON._id && (!/^[a-zA-Z0-9]{16}$/.test(npcJSON._id))) {    
+      delete npcJSON._id; // Entferne es, Foundry erstellt automatisch eine neue gültige ID    
+    }
     return npcJSON;
 
   } catch (e) {
